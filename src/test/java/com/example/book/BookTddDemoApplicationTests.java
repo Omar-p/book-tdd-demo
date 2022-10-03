a90234b22cabab6e33dc4d108c2676d612b4e5cd
Omar Shabaan <omar.plus.077@gmail.com>
2022-10-03 18:14:40 +0200

    change spring boot version to 3.0.0-M5

--- src/test/java/com/example/book/BookTddDemoApplicationTests.java
+++ src/test/java/com/example/book/BookTddDemoApplicationTests.java
@@ -3,9 +3,10 @@ package com.example.book;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ContextConfiguration(classes = BookTddDemoApplication.class)
+@SpringBootTest
 class BookTddDemoApplicationTests {
 
   @Test
